4b1e634d1d4f0007efc99d880e7ba5ff3e55c0dc
Benjamen Walsh <benwa@NITRO5>
2025-10-03 13:24:36 +1300

    changes

--- dist/assets/clone-DzwKBtFg.js
+++
@@ -1,1 +0,0 @@
-import{c as r}from"./graph-Cq_1Qb6V.js";var e=4;function a(o){return r(o,e)}export{a as c};
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: '.',
-  server: {
-    port: 5173
-  }
-});
+  base: '/mermaid-bpmn-plugin/',
+})
+
